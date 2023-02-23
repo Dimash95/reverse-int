c586d98d6e31d22f9e3de7babe812c2df49e5d44
Dimash <dinmukhamed.amirov@gmail.com>
2023-02-23 15:25:49 +0600

    reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+   let last;
+   let rev = 0;
+   let num = Math.abs(n);
+
+   while (num != 0) {
+      last = num % 10;
+      rev = rev * 10 + last;
+      num = Math.floor(num / 10);
+   }
+   if (num < 0) {
+      return -1 * rev
+   }
+   return rev;
+};
